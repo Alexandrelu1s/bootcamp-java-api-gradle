188d505d6249a8a5908b52f3c150fb8b0972abc3
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-19 18:23:43 -0300

    Create interface UserRepository

---
+++ src/main/java/alexandrelu1s/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package alexandrelu1s.domain.repository;
+
+import alexandrelu1s.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

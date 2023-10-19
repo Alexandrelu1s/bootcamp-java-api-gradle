99a54a8e9d657e1119630fb3a38a55b07519ccd2
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-19 16:37:06 -0300

    Start project

---
+++ src/main/java/alexandrelu1s/BootcampJavaApiGradleApplication.java
@@ -0,0 +1,13 @@
+package alexandrelu1s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJavaApiGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampJavaApiGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexandrelu1s/BootcampJavaApiGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package alexandrelu1s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampJavaApiGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
